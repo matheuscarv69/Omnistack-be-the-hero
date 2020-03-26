da0487fb4750e7862b4e6b9cd93ae8d1f99a4ffb
matheuscarv69 <matheus9126@gmail.com>
2020-03-26 17:14:09 -0400

    Back-end da aplicação

---
+++ backend/src/database/migrations/20200326115251_create_incidents.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('incidents', function(table){
+        // primary key
+        table.increments();
+        // campos da table
+        table.string('title').notNullable();
+        table.string('description').notNullable();
+        table.decimal('value').notNullable();
+        // relacionamento
+        table.string('ong_id').notNullable();
+        // key estrangeira
+        table.foreign('ong_id').references('id').inTable('ongs');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('incidents');
+};
